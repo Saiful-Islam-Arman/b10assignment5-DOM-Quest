4d26afa65cec673aca35a6f27c50bdddc8d92294
Saiful-Islam-Aman <saifulislamarmanr@gmail.com>
2024-09-24 20:48:26 +0600

    link the button dynamically

---
+++ js/features.js
@@ -0,0 +1,17 @@
+document.getElementById('btn-donate-container')
+    .addEventListener('click', function(){
+        showSectionById('donate-container');
+})
+
+// history container
+document.getElementById('btn-history-container')
+    .addEventListener('click', function(){
+        showSectionById('history-container');
+})
+
+// blog container
+// history container
+document.getElementById('btn-blog-container')
+    .addEventListener('click', function(){
+        showSectionById('blog-container');
+})
--- js/main.js
+++ js/main.js
@@ -8,6 +8,18 @@ document.getElementById('btn-donate1')
         const donateNkhali = getInputValueById('input-value-nkhali');
         const totalDonateCatch = getTextFieldValueById('donate-nkhali');
         const selfBalance = getTextFieldValueById('self-balance');
+
+        // value validation
+        if(isNaN(donateNkhali)){
+            alert("please Donte valid amount");
+            return;
+        }
+
+        // validation for 0 or negative value
+        if(donateNkhali <= 0){
+            alert("please Donte valid amount");
+            return;
+        }
         const totalDonateAmount = donateNkhali + totalDonateCatch;
         const selfBalanceTotal = selfBalance - donateNkhali;
         document.getElementById('donate-nkhali').innerText = totalDonateAmount;
@@ -22,6 +34,18 @@ document.getElementById('btn-donate2')
         const donateFeni = getInputValueById('input-value-feni');
         const TotalDonateCatch = getTextFieldValueById('donate-feni');
         const selfBalance = getTextFieldValueById('self-balance');
+
+         // value validation
+         if(isNaN(donateFeni)){
+            alert("please Donte valid amount");
+            return;
+        }
+
+        // validation for 0 or negative value
+        if(donateFeni <= 0){
+            alert("please Donte valid amount");
+            return;
+        }
         const totalDonateAmount = donateFeni + TotalDonateCatch;
         const selfBalanceTotal = selfBalance - donateFeni;
         document.getElementById('donate-feni').innerText = totalDonateAmount;
@@ -38,13 +62,36 @@ document.getElementById('btn-donate3')
         const donateQuota = getInputValueById('input-value-quota');
         const TotalDonateCatch = getTextFieldValueById('donate-quota');
         const selfBalance = getTextFieldValueById('self-balance');
+
+        // value validation
         if(isNaN(donateQuota)){
             alert("please Donte valid amount");
             return;
         }
+
+        // validation for 0 or negative value
+        if(donateQuota <= 0){
+            alert("please Donte valid amount");
+            return;
+        }
+
         const totalDonateAmount = donateQuota + TotalDonateCatch;
         const selfBalanceTotal = selfBalance - donateQuota;
         document.getElementById('donate-quota').innerText = totalDonateAmount;
         document.getElementById('self-balance').innerText = selfBalanceTotal;
 
     });
+
+
+// hide and show donate UI
+function showSectionById(id){
+
+    // hide all the section
+    document.getElementById('donate-container').classList.add('hidden');
+    document.getElementById('history-container').classList.add('hidden');
+    document.getElementById('blog-container').classList.add('hidden');
+
+    //show the section with provided id as perameter
+    document.getElementById(id).classList.remove('hidden');
+}
+
